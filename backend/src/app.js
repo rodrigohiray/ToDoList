9faf52a65eb95d45470eaea27381ce789272c76f
Rodrigo Passos <92394086+rodrigohiray@users.noreply.github.com>
2024-01-03 16:28:43 +0000

    Frontend
    criação frontend, estrutura e funcionalidades js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 //Importação do express e do router
 const express = require('express');
 const router = require('./router');
-
+const cors = require('cors');
 
 //Criação do app com uma instância do express
 const app = express();
 
 //Todas as requisições feitas ao app passarão pelo router
+app.use(cors()); //permite que a aplicação seja acessada 
 app.use(express.json());
 app.use(router);
 
